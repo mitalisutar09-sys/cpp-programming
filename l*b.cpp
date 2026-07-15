ea6ee26f6a29eb3f77bf846fa34f10b2f147e398
mitalisutar09-sys <mitalisutar09@gmail.com>
2026-07-15 10:57:38 +0530

    Add files via upload

---
+++ l*b.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int l,b;
+  cout<<"area of rectangle: ";
+  cin>>l>>b;
+  cout<<"area=  "<<l*b;
+}
---
+++ sub.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"enter two nos: ";
+  cin>>a>>b;
+  cout<<"sub= "<<a-b;
+}
